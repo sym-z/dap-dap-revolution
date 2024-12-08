b078232b50dc381f59d2a20882b2c65518da967b
SuperJRM <163477766+SuperJRM@users.noreply.github.com>
2024-12-07 18:55:21 -0800

    Phase Framework
    Worked on creating the framework for multiple choices, such as the type of greeting and dialogue choice. Also created the dialogue phase's framework.

---
+++ Dap Dap Revolution/Assets/Scripts/ButtonAction.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonAction : MonoBehaviour
+{
+    [SerializeField] private Button choiceButton;
+    [SerializeField] private GameObject GameManagement;
+    [SerializeField] private int buttonID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        choiceButton.onClick.AddListener(taskOnClick);
+    }
+
+
+    void taskOnClick()
+    {
+        GameManagement.GetComponent<GameManager>().continueButton(buttonID);
+        gameObject.SetActive(false);
+    }
+}
--- Dap Dap Revolution/Assets/Scripts/Game Manager.cs
+++ Dap Dap Revolution/Assets/Scripts/Game Manager.cs
@@ -1,73 +1,126 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using TMPro;
 using TMPro.Examples;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     private int phase = 1; //phase indicator
     private int score = 0;
-    public GameObject whatsUpText; 
-    public GameObject dapMeUpText;
+    public TextMeshProUGUI whatsUpText;
+    public GameObject opener;
     public GameObject ContinueButton;
-    public GameObject dapTarget;
-    public GameObject dapTargetRange;
-    public GameObject Arm;
     public GameObject GoodToSeeYouText;
+    public GameObject dapTarget;
     public bool dapHitCheck = false;
     public GameObject dapMissedText;
+    private GameObject[] phase1Objects;
+    public GameObject[] buttons;
+    private float difficulty = (1f);
 
-    public void continueButton()
+    private void Start()
     {
-        if (phase == 1)
+        buttons = GameObject.FindGameObjectsWithTag("choiceButton");
+        Activator(buttons, false);
+        phase1Objects = GameObject.FindGameObjectsWithTag("Phase1");
+        Activator(phase1Objects, false);
+    }
+
+    public void continueButton(int buttonID)
+    {
+        switch (phase)
         {
-            phase1();
+            case 1:
+                phase1(buttonID);
+                break;
+            case 2:
+                phase2(); 
+                break;
+             case 3:
+                phase3(buttonID);
+                break;
+            case 4:
+                phase4();
+                break;
+            default:
+                break;
         }
     }
 
-    private void phase1() //Show "Dap me up" plus button
+    private void phase1(int buttonID) //Show "Dap me up" plus button
     {
-        whatsUpText.SetActive(false);
-        dapMeUpText.SetActive(true);
-        dapTarget.SetActive(true);
-        dapTargetRange.SetActive(true);
-        Arm.SetActive(true);
+        opener.SetActive(false);
         ContinueButton.SetActive(false);
+        Activator(phase1Objects, true);
         phase += 1;
-        StartCoroutine(DapQTE());
+        StartCoroutine(DapQTE(buttonID));
+    }
+
+    private void phase2()   // Second Phase, asks player question
+    {
+        GoodToSeeYouText.SetActive(false);
+        dapMissedText.SetActive(false);
+        opener.SetActive(true);
+        whatsUpText.text = "You still on for Friday?";
+        Activator(buttons, true);
+        phase += 1;
+    }
+
+    private void phase3(int buttonID) // Finish button placement and insert dialogue choices here
+    {
+        switch (buttonID)
+        {
+            case 1:
+                whatsUpText.text = "Sounds great!";
+                score += 150;
+                ContinueButton.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void phase4()   // Begins goodbye QTE choice
+    {
+        // Insert Sonic Unleashed style QTE
     }
 
     public void dapHit() //Dap hit show text then on to phase 2
     {
-        dapTarget.SetActive(false);
-        dapTargetRange.SetActive(false);
-        dapMeUpText.SetActive(false);
+        Activator(phase1Objects, false);
         GoodToSeeYouText.SetActive(true);
         ContinueButton.SetActive(true);
         dapHitCheck = true;
-        Arm.SetActive(false);
         //score += 300; // Commented out for testing
     }
 
-    IEnumerator DapQTE() //dap qte show take away button plus show text after time
+    private void Activator(GameObject[] objList, bool positive)
     {
-        StartCoroutine(targetScale(dapTarget, 1.5f));
+        foreach(GameObject obj in objList)
+        {
+            obj.SetActive(positive);
+        }
+    }
+
+    IEnumerator DapQTE(int buttonID) //dap qte, removes button + shows text if failed
+    {
+        StartCoroutine(targetScale(dapTarget, 1.5f, buttonID));
         yield return new WaitForSeconds(1.5f);
-        dapTarget.SetActive(false);
-        dapTargetRange.SetActive(false);
-        dapMeUpText.SetActive(false);
+        Activator(phase1Objects, false);
         if (!dapHitCheck) //dap missed case
         {
             dapMissedText.SetActive(true);
             ContinueButton.SetActive(true);
-            Arm.SetActive(false);
         }
     }
 
-    IEnumerator targetScale(GameObject target, float timeLength)
+    //  Scales QTE target as time passes, granting points on time and greeting type
+    IEnumerator targetScale(GameObject target, float timeLength, int dapType)
     {
         float timeElapsed = 0;
         float targetTime = timeLength;
@@ -78,9 +131,17 @@ public class GameManager : MonoBehaviour
             target.transform.localScale = Vector3.Lerp(Vector3.zero, endScale*2, timeElapsed/targetTime);
             yield return null;
         }
+        // Changes timeframe based on difficulty of introduction
+        switch (dapType)
+        {
+            case 1:
+                difficulty = .5f; 
+                break;
+        }
+        // Calculates points on hit based on time taken
         if (dapHitCheck == true)
         {
-            float distanceRatio = Mathf.Abs((1/2) - (timeElapsed/targetTime));
+            float distanceRatio = Mathf.Abs(difficulty - (timeElapsed/targetTime));
             float ptsEarned = (-(Mathf.Pow(distanceRatio, 2f) * 300));
             score += (300 + (int)(ptsEarned));
             Debug.Log(score);
